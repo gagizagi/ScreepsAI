906f6ee08ccaa5c9d9bd0595ff4850e4e011bdb4
GazZy <gagizagi@gmail.com>
2017-04-29 20:55:05 +0200

    added jsdocs

--- Scripts/role.upgrader.js
+++ Scripts/role.upgrader.js
@@ -1,5 +1,12 @@
 let roleUpgrader = {
 
+    /**
+     * run - Base upgrader role function
+     * if creep has energy it will attempt to upgrade the controller
+     * else it will attempt to gather energy
+     *
+     * @param  {Creep} creep http://docs.screeps.com/api/#Creep
+     */
     run: function(creep) {
 
         // {bool} upgrading is a state in creep memory
