a28e3d7b24da7f6b111d3e0c72ee188dc0ee3a12
GazZy <gagizagi@gmail.com>
2017-04-29 18:30:07 +0200

    upload

---
+++ Scripts/main.js
@@ -0,0 +1,9 @@
+var roleHarvester = require('role.harvester')
+
+module.exports.loop = function () {
+    //Hi
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name]
+        roleHarvester.run(creep)
+    }
+}
--- main.js
+++
@@ -1,9 +0,0 @@
-var roleHarvester = require('role.harvester');
-
-module.exports.loop = function () {
-    //Hi
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        roleHarvester.run(creep);
-    }
-}
