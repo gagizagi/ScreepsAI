0171f4382623ea3ed7e396bd6703009e2f9205e1
GazZy <gagizagi@gmail.com>
2017-04-29 20:45:18 +0200

    Added upgrader script

---
+++ Scripts/role.upgrader.js
@@ -0,0 +1,36 @@
+let roleUpgrader = {
+
+    run: function(creep) {
+
+        // {bool} upgrading is a state in creep memory
+        // TRUE     - creep attempts to upgrade controller
+        // FALSE    - creep attempts to gather energy
+        // If creep is upgrading but runs ouf of energy set to false
+        // If creep is not upgrading but has full energy set to true
+        if(creep.memory.upgrading && creep.carry.energy == 0)
+            creep.memory.upgrading = false
+        if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity)
+            creep.memory.upgrading = true
+
+        // If creep is upgrading
+        if(creep.memory.upgrading) {
+            // Attempt to upgrade controller
+            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                // If not in range of controller, move to it
+                creep.moveTo(creep.room.controller /* TODO add color */)
+            }
+        }
+        // If not upgrading
+        else {
+            // Get an array of energy sources
+            let sources = creep.room.find(FIND_SOURCES)
+            // Attempt to harvest 0th energy resource
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                // If not in range of 0th energy resource, move to it
+                creep.moveTo(sources[0] /* TODO add color */)
+            }
+        }
+    }
+}
+
+module.exports = roleUpgrader
