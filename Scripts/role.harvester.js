b37ea073cf2026d39a8b2a87ad2c32c1b60ca25c
GazZy <gagizagi@gmail.com>
2017-04-29 20:30:30 +0200

    Added harvester script

--- Scripts/role.harvester.js
+++ Scripts/role.harvester.js
@@ -1,15 +1,44 @@
-var roleHarvester = {
+let roleHarvester = {
 
+    /**
+     * run - Base harvester role function
+     * if creep has energy it will attempt to transfer it to structures
+     * else if will attempt to gather energy
+     *
+     * @param  {Creep} creep http://docs.screeps.com/api/#Creep
+     */
     run: function(creep) {
+        // If creep is not on full energy
         if(creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_SOURCES)
+            // Get array of energy sources
+            let sources = creep.room.find(FIND_SOURCES)
+            // Try to harvest the energy source
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE)
-                creep.moveTo(sources[0])
+                // If not in range to harvest energy source, move to it
+                creep.moveTo(sources[0] /* TODO add color */)
         }
+        // If creep is on full energy
         else {
-            var home = Game.spawns['Spawn1']
-            if(creep.transfer(home, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
-                creep.moveTo(home)
+            // Get array of structures that require energy
+            let targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return ( // Filter structure types
+                        structure.structureType == STRUCTURE_EXTENSION ||
+                        structure.structureType == STRUCTURE_SPAWN ||
+                        structure.structureType == STRUCTURE_TOWER)
+                        // Filter out structures with full energy
+                        && structure.energy < structure.energyCapacity
+                }
+            })
+
+            // If targets array is not empty
+            if(targets.length > 0) {
+                // Attempt to transfer energy from creep to 0th target
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    // If not in range of 0th target move to it
+                    creep.moveTo(targets[0] /* TODO add color */)
+                }
+            }
         }
     }
 
