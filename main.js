d6af15aa83450e45356bacc9d13a134985bd7cdd
GazZy <gagizagi@gmail.com>
2017-04-29 03:59:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var roleHarvester = require('role.harvester');
 
 module.exports.loop = function () {
-
+    //Hi
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         roleHarvester.run(creep);
