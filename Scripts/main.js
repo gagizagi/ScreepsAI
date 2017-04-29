948899768dfd9f690be8f3131fcac24ef114d08c
GazZy <gagizagi@gmail.com>
2017-04-30 01:08:17 +0200

    switched to switch :p

--- Scripts/main.js
+++ Scripts/main.js
@@ -3,16 +3,24 @@ let roleUpgrader = require('role.upgrader')
 let roleBuilder = require('role.builder')
 
 module.exports.loop = function () {
+
+    // Loop over all Creeps
     for(let name in Game.creeps) {
+        // This creep
         let creep = Game.creeps[name]
-        if(creep.memory.role == 'harvester') {
+
+        // Run function for this crap based on its role
+        switch(creep.memory.role) {
+        case 'harvester':
             roleHarvester.run(creep)
-        }
-        if(creep.memory.role == 'upgrader') {
+            break
+        case 'upgrader':
             roleUpgrader.run(creep)
-        }
-        if(creep.memory.role == 'builder') {
+            break
+        case 'builder':
             roleBuilder.run(creep)
+            break
         }
+
     }
 }
