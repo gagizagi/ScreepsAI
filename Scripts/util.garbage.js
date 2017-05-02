e2ef15bd03b29734ac7c83612e410513807ab2c1
GazZy <gagizagi@gmail.com>
2017-05-02 21:52:51 +0200

    Added comments

--- Scripts/util.garbage.js
+++ Scripts/util.garbage.js
@@ -1,8 +1,14 @@
 let garbageCollector = {
 
+    /**
+     * Deletes memory of objects that no longer exist
+     */
     run: () => {
+        // Loop over all creep objects stored in memory
         for(var name in Memory.creeps) {
+            // If the creep object exists in memory but not in game
             if(!Game.creeps[name]) {
+                // Delete creep object in memory
                 delete Memory.creeps[name]
             }
         }
